e5ef4dc70dec22ea8bd66cb8291c442b8ec36a4b
David-gsm123 <christiandavegesim1304@gmail.com>
2025-11-29 02:25:06 +0800

    Fix[Gesim]: Fixed title and priority labels

--- notification.php
+++ notification.php
@@ -16,6 +16,7 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])){
         $department_id = $_SESSION['department_id'] ?? null;
         $taskData = get_notifications($conn, $user_id);
     }
+    // print_r($taskData);
 
     
 ?>
@@ -37,7 +38,7 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])){
         <div class="notification-container">
             <div class="tabs">
                 <div class="tab active" data-tab="tasks">Tasks Updates</div>
-                <div class="tab" data-tab="comments">Comments</div>
+              
             </div>
 
             <div class="notification-list" id="notificationList"></div>
@@ -56,45 +57,6 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])){
 
     <script>
         const tasksData = <?php echo json_encode($taskData); ?>;
-        const commentsData = [
-            {
-                type: 'comment',
-                title: 'Logo Design Feedback',
-                comment: 'Great work on the initial design! Could you try using our brand colors more prominently? Also, the font might need to be more modern.',
-                time: '11:40 AM'
-            },
-            {
-                type: 'comment',
-                title: 'Homepage Mockup',
-                comment: 'The new layout looks fantastic! I love the clean design. Can we add more whitespace around the hero section?',
-                time: '10:55 AM'
-            },
-            {
-                type: 'comment',
-                title: 'Q3 Report Numbers',
-                comment: 'Please double-check the revenue figures for September. They seem slightly off compared to our internal records.',
-                time: '09:30 AM'
-            },
-            {
-                type: 'comment',
-                title: 'Meeting Agenda',
-                comment: 'Can we also discuss the budget allocation for next quarter during the meeting? It\'s quite urgent.',
-                time: '08:50 AM'
-            },
-            {
-                type: 'comment',
-                title: 'Campaign Analytics',
-                comment: 'The engagement rate is impressive! Let\'s discuss how we can replicate this success in future campaigns.',
-                time: '02:45 PM'
-            },
-            {
-                type: 'comment',
-                title: 'Handbook Updates',
-                comment: 'Don\'t forget to include the new remote work policy and updated vacation guidelines in the handbook.',
-                time: '03:15 PM'
-            }
-        ];
-
         let currentTab = 'tasks';
 
         function renderNotifications() {
@@ -102,10 +64,10 @@ if (isset($_SESSION['role']) && isset($_SESSION['id'])){
             const data = currentTab === 'tasks' ? tasksData : commentsData;
             
             list.innerHTML = data.map((item, index) => `
-                <div class="notification-item" onclick="openModal(${index})">
+                <div class="notification-item ${item.is_read == 0 ? 'read' : ''}" onclick="openModal(${index})">
                     <img class="notification-icon" src="images/noti.png" alt="Notifications">
                     <div class="notification-text">
-                        <strong>New ${currentTab === 'tasks' ? 'task' : 'comment'} ${currentTab === 'tasks' ? 'assigned' : 'received'}: "${item.task_title}"</strong>. Click here to view it. (${item.task_due_date})
+                        <strong>New ${currentTab === 'tasks' ? 'task' : 'comment'} ${currentTab === 'tasks' ? 'assigned' : 'received'}: "${item.task_title}"</strong>. Click here to view it. (${item.created_At.split(" ")[0]})
                     </div>
                 </div>
             `).join('');
