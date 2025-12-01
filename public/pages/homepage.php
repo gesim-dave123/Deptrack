f2acfc7080a3aeb4c3a28ca9effcc963ce32ef3c
David-gsm123 <christiandavegesim1304@gmail.com>
2025-12-02 03:40:49 +0800

    Fix[Gesim]: Fixed issues in headers and links

--- public/pages/homepage.php
+++ public/pages/homepage.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DepTrack - Streamline Team Productivity</title>
-    <link rel="stylesheet" href="../styles/homepage.css">
+    <link rel="stylesheet" href="../styles/homepage.css?v=.0">
 </head>
 <body>
     <header>
         <div class="logo">
             <div class="logo-icon">
-                <img src ="../images/logo.png">
+                <img src ="public\images\logo.png">
             </div>
             <span>DepTrack</span>
         </div>
@@ -21,7 +21,7 @@
             <a href="#help">Help</a>
         </nav>
     </header>  
-    <section class="hero">
+    <section class="hero">  
         <div class="hero-content">
             <h1>
                 <span class="green-text">Streamline</span>
@@ -30,11 +30,11 @@
                 <span class="dark-text">team productivity</span>
             </h1>
             <p>Manage tasks effortlessly, track progress in real-time, and collaborate with your team using our powerful yet simple task management solution.</p>
-           <button class="cta-button" onclick="window.location.href='login.php'">Get Started</button>
+           <button class="cta-button" onclick="window.location.href='public/pages/login.php'">Get Started</button>
         </div>
 
         <div class="image">
-            <img src="../images\HomepageImage.png" id="homeImg">
+            <img src="public\images\HomepageImage.png" id="homeImg">
         </div>
     </section>
 </body>
